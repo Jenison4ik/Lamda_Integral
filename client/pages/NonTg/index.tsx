c45f417c6ee09753defbb57f57e34a6bf302a976
Jenison4ik <superjoni121@gmail.com>
2026-01-28 12:01:26 +1000

    Refactor component paths in configuration, update CSS file references, and enhance NonTg page with new UI components and styles. Add new dependencies for improved functionality and remove unused SCSS files.

--- client/pages/NonTg/index.tsx
+++ client/pages/NonTg/index.tsx
@@ -1,4 +1,6 @@
+import DarkVeil from "@/components/DarkVeil";
 import styles from "./style.scss";
+import { Button } from "@/components/ui/button";
 import { RiTelegram2Fill } from "react-icons/ri";
 export default function NonTg() {
   function handleClick() {
@@ -6,16 +8,34 @@ export default function NonTg() {
   }
 
   return (
-    <div>
-      <h1>Lamda Integrall</h1>
-      <p>
-        Это телеграм бот для тренировки по интегрированию, вы можете опробовать
-        его прямо сейчас
-      </p>
-      <button onClick={handleClick}>
-        <RiTelegram2Fill />
-        Попробовать сейчас
-      </button>
+    <div className="relative min-h-screen w-screen text-white">
+      <div className="fixed inset-0 -z-10">
+        <DarkVeil
+          hueShift={0}
+          noiseIntensity={0}
+          scanlineIntensity={0}
+          speed={0.5}
+          scanlineFrequency={0}
+          warpAmount={0}
+          resolutionScale={1}
+        />
+      </div>
+      <div className="relative flex flex-col items-center justify-center gap-4 h-screen w-screen">
+        <h1 className="font-bold text-6xl">Lamda Integrall</h1>
+        <p>
+          Это телеграм бот для тренировки по интегрированию, вы можете
+          опробовать его прямо сейчас
+        </p>
+        <Button
+          onClick={handleClick}
+          className={
+            "gap-2 w-auto cursor-pointer hover:scale-105 transition-all duration-300 bg-white text-black hover:bg-white/80"
+          }
+        >
+          <RiTelegram2Fill className="size-4" />
+          Попробовать сейчас
+        </Button>
+      </div>
     </div>
   );
 }
