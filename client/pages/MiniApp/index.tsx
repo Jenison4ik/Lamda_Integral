76d8cd2c0ef88550cfae2d48c17686d2d46f017f
Jenison4ik <superjoni121@gmail.com>
2026-01-29 21:33:42 +1000

    Enhance MiniApp component layout and styling
    - Refactored the MiniApp component to improve structure and readability.
    - Added CardHeader and CardContent for better content organization.
    - Updated user greeting to dynamically display the user's name and photo.
    - Adjusted global font size in CSS from 18px to 16px for improved readability.

--- client/pages/MiniApp/index.tsx
+++ client/pages/MiniApp/index.tsx
@@ -1,16 +1,20 @@
 import { AppProvider } from "@/providers/AppContex";
 import useHaptic from "@/hooks/useHaptic";
 import { useEffect, useMemo } from "react";
-import { retrieveLaunchParams, swipeBehavior, viewport } from "@tma.js/sdk-react";
-import { Card } from "@/components/ui/card";
-import "./style.css"
+import {
+  retrieveLaunchParams,
+  swipeBehavior,
+  viewport,
+} from "@tma.js/sdk-react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import "./style.css";
 
-export default function MiniApp(){
-    const { hapticTrigger } = useHaptic();
-    const launchParams = useMemo(() => retrieveLaunchParams(), []);
+export default function MiniApp() {
+  const { hapticTrigger } = useHaptic();
+  const launchParams = useMemo(() => retrieveLaunchParams(), []);
 
-    //ВЫставление настроек
-    useEffect(()=>{
+  //ВЫставление настроек
+  useEffect(() => {
     swipeBehavior.mount();
     swipeBehavior.disableVertical();
 
@@ -24,15 +28,29 @@ export default function MiniApp(){
         console.error(e);
       }
     };
-    },[])
+  }, []);
 
-
-    return (
-        <AppProvider>
-            <main>
-            <Card><div>Ваш контент здесь</div></Card>
-            </main>
-        </AppProvider>
-    )
+  return (
+    <AppProvider>
+      <main>
+        <Card>CardContent</Card>
+        <CardHeader className="flex items-start gap-2">
+          <img
+            src={
+              launchParams?.tgWebAppData?.user?.photo_url ??
+              "./img/user_placeholder.jpeg"
+            }
+            alt="user"
+            className="w-[40px] h-[40px] rounded-full"
+          />
+          <CardTitle>
+            Привет 👋, {launchParams?.tgWebAppData?.user?.first_name}!
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div>Ваш контент здесь</div>
+        </CardContent>
+      </main>
+    </AppProvider>
+  );
 }
-
