7cfce752ee1a318f204cf2c72276d8440c14b08f
Evegeniy <109351108+Jenison4ik@users.noreply.github.com>
2026-01-30 02:39:46 +1000

    New Load Screen

--- client/pages/MiniApp/LoadScreen.tsx
+++ client/pages/MiniApp/LoadScreen.tsx
@@ -1,7 +1,10 @@
 import { Spinner } from "@/components/ui/spinner"
 
 export default function LoadScreen(){
-    return <div className="w-full h-full flex items-center justify-center">
-        <div><Spinner data-icon="inline-start" className="size-8"/>Загрузка</div>
+    return <div className="min-h-screen bg-background text-foreground flex items-center justify-center">
+        <div className="flex items-center gap-3">
+            <Spinner className="size-8"/>
+            <span>Загрузка</span>
+        </div>
     </div>
 }
